e948bddbf6a1a898d81e084e42888173faa0c818
Leonardo Galindo <lgalin6@illinois.edu>
2022-12-13 23:28:04 -0600

    add authentication

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,7 +2,7 @@ import os
 import logging
 from config_flask import Config
 
-from flask import Flask
+from flask import Flask, session
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -10,12 +10,12 @@ from flask_jwt_extended import JWTManager
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_mongoengine import MongoEngine
+from authlib.integrations.flask_client import OAuth
 
 from api.core import all_exception_handler
 
 from redis import Redis
 import rq
-import tweepy
 
 from logging.handlers import RotatingFileHandler
 
@@ -29,6 +29,7 @@ limiter = Limiter(
     storage_uri=os.environ.get("REDIS_URL"),
     strategy="fixed-window",
 )
+oauth = OAuth()
 
 from transformers import pipeline
 
@@ -55,8 +56,19 @@ def create_app(config_class=Config):
 
     app = Flask(__name__)
     app.config.from_object(config_class)
+    app.secret_key = "POTATOES"
     app.redis = Redis.from_url(app.config.get("REDIS_URL", "redis://"))
     app.task_queue = rq.Queue("flask-api-queue", connection=app.redis)
+    oauth.register(
+        name="twitter",
+        client_id=app.config.get("TWITTER_API_KEY"),
+        client_secret=app.config.get("TWITTER_API_SECRET"),
+        api_base_url="https://api.twitter.com/1.1/",
+        request_token_url="https://api.twitter.com/oauth/request_token",
+        access_token_url="https://api.twitter.com/oauth/access_token",
+        authorize_url="https://api.twitter.com/oauth/authenticate",
+        fetch_token=lambda: session.get("token"),  # DON'T DO IT IN PRODUCTION
+    )
 
     with app.app_context():
         db.init_app(app)
@@ -66,22 +78,16 @@ def create_app(config_class=Config):
         cors.init_app(app)
         limiter.init_app(app)
 
-        # Setup Twitter API
-        twitter_auth = {
-            "bearer_token": app.config.get("TWITTER_BEARER"),
-            "consumer_key": app.config.get("TWITTER_API_KEY"),
-            "consumer_secret": app.config.get("TWITTER_API_SECRET"),
-            "access_token": app.config.get("TWITTER_ACCESS_TOKEN"),
-            "access_token_secret": app.config.get("TWITTER_ACCESS_SECRET"),
-        }
-        app.config["TWITTER_CLIENT"] = tweepy.Client(**twitter_auth)
+        oauth.init_app(app)
 
     # import and register blueprints
     from api.views.main import bp as main_bp
     from api.views.app_blueprint import bp as app_blueprint_bp
+    from api.views.auth import bp as auth_bp
 
     # why blueprints http://flask.pocoo.org/docs/1.0/blueprints/
     app.register_blueprint(app_blueprint_bp)
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(main_bp, url_prefix="/api")
 
     # Set the rate limit for all routes in the auth_bp blueprint to 1 per second
